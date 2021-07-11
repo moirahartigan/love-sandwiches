51e907a2a6ea309f2b948583ed463b96e7498b96
moirahartigan <moirahartigan@gmail.com>
2021-07-11 19:57:22 +0000

    correct spelling error

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-# python code goes here
 import gspread
 from google.oauth2.service_account import Credentials
-
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,21 +16,21 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
-    Run a while look to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers seperated
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, seperated by commas.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here:\n")
-        
-        sales_data = data_str.split(",")        
+
+        sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Data is Valid!")
+            print("Data is valid!")
             break
 
     return sales_data
@@ -48,7 +46,7 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 Values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
@@ -82,9 +80,9 @@ def validate_data(values):
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
-    update the relevant worksheet with the data provided
+    Update the relevant worksheet with the data provided
     """
-    print(f"updating {worksheet} worksheet...\n")
+    print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
@@ -93,7 +91,6 @@ def update_worksheet(data, worksheet):
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
-
     The surplus is defined as the sales figure subtracted from the stock:
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
@@ -106,18 +103,18 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
 
 
 def get_last_5_entries_sales():
     """
-    Collects collums of data from sales worksheet, collecting
+    Collects columns of data from sales worksheet, collecting
     the last 5 entries for each sandwich and returns the data
-    as a list of lists
+    as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-   
+
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
@@ -130,7 +127,7 @@ def calculate_stock_data(data):
     """
     Calculate the average stock for each item type, adding 10%
     """
-    print("calculating stock data...\n")
+    print("Calculating stock data...\n")
     new_stock_data = []
 
     for column in data:
@@ -151,12 +148,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-    sales_colums = get_last_5_entries_sales()
-    stock_data = calculate_stock_data(sales_colums)
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
-
